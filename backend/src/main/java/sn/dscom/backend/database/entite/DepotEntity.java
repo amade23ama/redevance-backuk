29c83744600f039b7eaa429b07b235cd9dddbbf2
amade.diouf <sully!2020*>
2023-04-18 20:11:37 +0200

    creation de la partie backend

---
+++ backend/src/main/java/sn/dscom/backend/database/entite/DepotEntity.java
@@ -0,0 +1,13 @@
+package sn.dscom.backend.database.entite;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepotEntity {
+    // todo les attribut
+    private Long id;
+}
---
+++ backend/src/main/java/sn/dscom/backend/database/entite/UtilisateurEntity.java
@@ -0,0 +1,14 @@
+package sn.dscom.backend.database.entite;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UtilisateurEntity {
+
+    // todo les attributs
+    private Long id;
+}
